3676b9939db5a20ba7624b2f0a48e1a491d78e9a
gollum <facugm>
2018-09-04 11:00:33 -0300

    Desarrollo de app

--- Application/ex_app.js
+++ Application/ex_app.js
@@ -1,21 +1,68 @@
 
 var express = require('express');
 var router  = express.Router();
-const url   = require('url');
+var url_biblio   = require('url');
 var app     = express();
  
 // Conexión
 var database = require('/home/nemo/Documentos/Nemo_Pasantias/Application/mysql');
 
+//Raíz
 app.get('/', function (req, res) {
-    res.send('Hola!');
+    res.send('/faqtag (/tag) <br> /faqfile (/article_name) <br> /faqtrel <br> /user');
   });
  
-// Select
-app.get('/select', function(req, res, err) {
-    var T_TAG_val   = req.query.tag;
+// SELECT ALL FAQ_TAG 
+  app.get('/faqtag', function(req, res, err) {
+    var sql = "SELECT * FROM FAQ_TAG";
+    database.connection.query(sql, function(err, result, fields) {
+        if(err) throw res.json(err);
+        res.json(result);
+    });
+});
+
+// SELECT con parámetros FAQ_TAG
+app.get('/faqtag/:tag', function(req, res, err) {
+    var param   = req.params.tag;
     var sql = "SELECT * FROM FAQ_TAG WHERE TAG = ?";
-    database.connection.query(sql,[T_TAG_val], function(err, result, fields) {
+    database.connection.query(sql,[param], function(err, result, fields) {
+        if(err) throw res.json(err);
+        res.json(result);
+    });
+});
+
+// SELECT ALL FAQ_TAG 
+app.get('/faqfile', function(req, res, err) {
+    var sql = "SELECT * FROM FAQ_FILE";
+    database.connection.query(sql, function(err, result, fields) {
+        if(err) throw res.json(err);
+        res.json(result);
+    });
+});
+
+// SELECT con parámetros FAQ_FILE
+app.get('/faqfile/:a_name', function(req, res, err) {
+    var param = req.params.a_name;
+    var sql = "SELECT * FROM FAQ_FILE WHERE ARTICLE_NAME = ?";
+    database.connection.query(sql,[param], function(err, result, fields) {
+        if(err) throw res.json(err);
+        res.json(result);
+    });
+});
+
+// SELECT ALL FILE_TAG_RELATION
+app.get('/faqtrel', function(req, res, err) {
+    var sql = "SELECT * FROM FILE_TAG_RELATION";
+    database.connection.query(sql, function(err, result, fields) {
+        if(err) throw res.json(err);
+        res.json(result);
+    });
+});
+
+// SELECT ALL USSER_ACCOUNT
+app.get('/user', function(req, res, err) {
+    var sql = "SELECT * FROM USER_ACCOUNT";
+    database.connection.query(sql, function(err, result, fields) {
         if(err) throw res.json(err);
         res.json(result);
     });
