f7c32626ac43566f260237ac80e35693f3676f29
gollum <facugm>
2018-09-07 13:01:47 -0300

    Integracion models

--- Application/Ex_App/app/controllers/home.js
+++ Application/Ex_App/app/controllers/home.js
@@ -1,16 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
+//const Sequelize = require('sequelize');
+//const sequelize = new Sequelize('mysql://developer:nemonemo@10.45.0.87:3306/FAQS_DEV');
 
 module.exports = (app) => {
   app.use('/', router);
+  app.use('/faqtag',router);
 };
 
 router.get('/', (req, res, next) => {
   db.Article.findAll().then((articles) => {
     res.render('index', {
-      title: 'Generator-Express MVC',
+      title: 'Welcome',
       articles: articles
     });
   });
+});
+
+router.get('/faqtag', (req, res, next) => {
+  const User = db.sequelize.define('USER_ACCOUNT',{
+    user_id : db.Sequelize.STRING,
+    user_password : db.Sequelize.STRING
+  })
+  /*
+  var param   = req.params.tag;
+  db.sequelize.query("SELECT * FROM FAQ_TAG WHERE TAG = ?", { raw: false, replacements: [param]}
+  ).spread(myTableRows => {
+    res.json(myTableRows)
+  });*/
+  User.findAll().spread(resultado => {
+    res.json(resultado)
+  })
 });
