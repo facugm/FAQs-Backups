e79513fc2c2a5af8f2ac534a5de3d9e50cab4089
Sofiamonza <sofimonza2015@gmail.com>
2018-09-12 12:08:24 -0300

    Modifique index

--- Application/Ex_App/app/controllers/home.js
+++ Application/Ex_App/app/controllers/home.js
@@ -1,36 +1,81 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
-//const Sequelize = require('sequelize');
-//const sequelize = new Sequelize('mysql://developer:nemonemo@10.45.0.87:3306/FAQS_DEV');
+const Sequelize = require('sequelize');
 
 module.exports = (app) => {
   app.use('/', router);
-  app.use('/faqtag',router);
+  //app.use('/faqtag',router);
+
 };
 
+/*
 router.get('/', (req, res, next) => {
+
+  const USER_ACCOUNT = db.sequelize.define('USER_ACCOUNT', {
+    USER_ID: Sequelize.VARCHAR,
+    USER_PASSWORD: Sequelize.VARCHAR
+  })
+  
+
+  USER_ACCOUNT.findAll({
+    
+    where:{
+      USER_PASSWORD: 123456
+    }
+  }).spread(MyTableRows =>{
+
+    res.json(MyTableRows)
+  });
+});*/
+/*
+router.get('/', (req, res, next) => {
+
   db.Article.findAll().then((articles) => {
     res.render('index', {
-      title: 'Welcome',
+      title: "HOLA",
+      text: "blabalbdlabdasl",
       articles: articles
     });
-  });
+
 });
+});
+*/
+//router.get('/faqtag', (req, res, next) => {
 
-router.get('/faqtag', (req, res, next) => {
-  /*const User = db.sequelize.define('USER_ACCOUNT',{
-    user_id : db.Sequelize.STRING,
-    user_password : db.Sequelize.STRING
-  })*/
-  
-  var param   = req.params.tag;
-  db.sequelize.query("SELECT * FROM FAQ_TAG"
-  ).spread(myTableRows => {
-    res.json(myTableRows)
+ // db.User.findAll().then((myTableRows) => {
+ //res.json(myTableRows)
+ //});
+
+   /*
+    db.Article.findAll().then((articles) => {
+      res.render('index', {
+        title: myTableRows,
+        articles: articles
+      });
+    });
+    */
+
+
+/*
+router.get('/faqtag/:name', (req, res, next) => {
+
+  var tag_name= req.params.name;
+
+  db.sequelize.query("SELECT * FROM FAQ_TAG WHERE TAG_NAME= ?", {raw: false, replacements: [tag_name]}
+).spread(myTableRows => {
+
+   res.json(myTableRows)
+
   });
 
-  /*User.findAll().spread(resultado => {
-    res.json(resultado)
-  })*/
+});
+*/
+
+router.get('/', (req, res, next) => {
+
+    res.render('index', {
+      titulo: "BIENVENIDO"
+    });
+
 });
