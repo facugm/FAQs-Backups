0b2af439018d70ae6ef40a5b4b00d36d8ebaed15
Sofiamonza <sofimonza2015@gmail.com>
2018-10-24 12:25:34 -0300

    Creacion de la pagina article_page

--- Application/Ex_App/app/controllers/home.js
+++ Application/Ex_App/app/controllers/home.js
@@ -20,39 +20,38 @@ router.get('/', (req, res, next) => {
 });
 */
 
-router.get('/faqtag', (req, res, err) => {
-  res.render('landing_page',{
-  });
-});
-
-router.get('/search', (req, res, err) => {
-
- /*
-  db.FAQ_FILE.findAll({attributes: ['ARTICLE_NAME'], order: "VISITS"}).then((list_articles) => {
-
-
-    res.render('landing_page', {
-      list_articles : list_articles
-    });
-
-});*/
-
-var pattern = req.query.term;
-console.log("PATRON", pattern);
-db.FAQ_FILE.findAll({attributes: ['ARTICLE_NAME', 'FAQ_FILE_ID'], limit: 10, order: ['ARTICLE_NAME'], where: { ARTICLE_NAME:{$like: "%" + pattern + "%" }}}).then((response)=>{
-  // PROCESAR PARA RETORNAR LO QUE SE DESEA
-  var list = response.map(function(ele){
-    return { id: ele.FAQ_FILE_ID, value: ele.ARTICLE_NAME, label: ele.ARTICLE_NAME };
-  })
-  res.json(list);
-});
-
-});
-
-
-
 router.get('/', (req, res, next) => {
   res.render('landing_page', {
     titulo: "BIENVENIDO"
   });
 });
+
+router.get('/article/:term', (req, res, next) => {
+  var pattern = req.params.term;
+  db.FAQ_FILE.findOne({attributes: ['ARTICLE_NAME', 'ARTICLE_TEXT'], where: { FAQ_FILE_ID:  pattern }}).then((response)=>{
+    console.log("PATRON", pattern);
+
+    res.render('article_page', {
+      titulo: response.ARTICLE_NAME,
+      texto: response.ARTICLE_TEXT 
+    });
+  })
+  
+});
+
+router.get('/search', (req, res, err) => { 
+
+    
+  var pattern = req.query.term;
+  console.log("PATRON", pattern);
+  db.FAQ_FILE.findAll({attributes: ['ARTICLE_NAME', 'FAQ_FILE_ID'], limit: 10, order: ['ARTICLE_NAME'], where: { ARTICLE_NAME:{$like: "%" + pattern + "%" }}}).then((response)=>{
+    console.log("PATRON", pattern);
+  var list = response.map(function(ele){
+  return { id: ele.FAQ_FILE_ID, value: ele.ARTICLE_NAME, label: ele.ARTICLE_NAME };
+  })
+  res.json(list);
+  });});
+
+
+
+
--- Application/Ex_App/public/js/autocompletar.js
+++ Application/Ex_App/public/js/autocompletar.js
@@ -12,40 +12,9 @@ $(function() {
           res(data);
         }); 
       },
-      minLength: 1,
+      minLength: 3,
       select: function( event, ui ) {
         log( "Selected: " + ui.item.value + " ID " + ui.item.id );
       }
     });
 });
-/*
-$(function() {
-
-  function log( message ) {
-    $( "<div>" ).text( message ).prependTo( "#log" );
-    $( "#log" ).scrollTop( 0 );
-  }
-
-  $( "#articles" ).autocomplete({
-
-    source: function(req, res){
-
-      /*$.getJSON( "/search", req, function( data, status, xhr ) {  
-
-        res(data.slice(0, limit));
-
-      });
-      var rta = ["Buscar","Buscar2","Buscar3","Buscar4","Buscar5","Buscar6"];
-      res(rta);
-
-    },
-
-    minLength: 3,
-    select: function( event, ui ) {
-      log( "Selected: " + ui.item.value + " ID " + ui.item.id + " Label: " + ui.item.label);
-    }
-
-  });
-
-});
-*/
