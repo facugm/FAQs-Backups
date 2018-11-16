43b1f32a850072ee634026da5fd7c51d5eda020d
gollum <facugm>
2018-11-16 10:11:52 -0300

    Datos de tabla en input (incompleto)

--- Application/Ex_App/app/controllers/home.js
+++ Application/Ex_App/app/controllers/home.js
@@ -27,12 +27,33 @@ router.get('/', (req, res, next) => {
   });
 });
 
-router.get('/edit/', (req, res, next) => {
+router.get('/edit/:term', (req, res, next) => {
+  var pattern = req.params.term;
+  db.FAQ_FILE.findOne({attributes: ['ARTICLE_NAME', 'ARTICLE_TEXT'], where: { FAQ_FILE_ID:  pattern }}).then((response)=>{
+    db.sequelize.query('SELECT R_FAQ_FILE_ID, ARTICLE_NAME FROM FAQ_FILE_FILE ff INNER JOIN FAQ_FILE fi ON ff.R_FAQ_FILE_ID = fi.FAQ_FILE_ID WHERE L_FAQ_FILE_ID = :key', {replacements: {key: pattern}, type: db.sequelize.QueryTypes.SELECT }).then(rta => {
+      db.sequelize.query('SELECT t.TAG_NAME FROM FAQ_TAG t INNER JOIN FAQ_FILE_TAG ft ON ft.`FAQ_T_ID`= t.`FAQ_TAG_ID` INNER JOIN FAQ_FILE ff ON ff.`FAQ_FILE_ID` = ft.`FAQ_F_ID` WHERE ff.`FAQ_FILE_ID` = :key', {replacements: {key: pattern}, type: db.sequelize.QueryTypes.SELECT}).then(respuesta =>{
+
+        var lista = respuesta.map( item => {
+          return { tag: item['TAG_NAME']}     
+          })
+    
+      var list = rta.map( item => {
+        return { article: item['ARTICLE_NAME'], id: item['R_FAQ_FILE_ID']}     
+        });
+
+  console.log(response.ARTICLE_NAME);
+
   res.render('edit_page', {
-    titulo: "Editar artículo"
+    titulo: "Editar artículo",
+    article_name: response.ARTICLE_NAME,
+    texto: response.ARTICLE_TEXT,
+    tag: lista,
+    related: list
+    });
+  });
   });
 });
-
+});
 /*router.get('/article/:term', (req, res, next) => {
   var pattern = req.params.term;
   db.FAQ_FILE.findOne({attributes: ['ARTICLE_NAME', 'ARTICLE_TEXT'], where: { FAQ_FILE_ID:  pattern }}).then((response)=>{
@@ -46,7 +67,7 @@ router.get('/edit/', (req, res, next) => {
   
 });*/
 
-router.get('/article/:term', (req, res, next) => {  
+router.get('/article/:term', (req, res, next) => {
   var pattern = req.params.term;
   db.FAQ_FILE.findOne({attributes: ['ARTICLE_NAME', 'ARTICLE_TEXT'], where: { FAQ_FILE_ID:  pattern }}).then((response)=>{
     db.sequelize.query('SELECT R_FAQ_FILE_ID, ARTICLE_NAME FROM FAQ_FILE_FILE ff INNER JOIN FAQ_FILE fi ON ff.R_FAQ_FILE_ID = fi.FAQ_FILE_ID WHERE L_FAQ_FILE_ID = :key', {replacements: {key: pattern}, type: db.sequelize.QueryTypes.SELECT }).then(rta => {
@@ -71,14 +92,10 @@ router.get('/search', (req, res, err) => {
  db.sequelize.query("SELECT f. FAQ_FILE_ID, f. ARTICLE_NAME,f. ARTICLE_TEXT FROM FAQ_TAG t INNER JOIN FAQ_FILE_TAG ft ON ft.`FAQ_T_ID` =  t.`FAQ_TAG_ID` INNER JOIN FAQ_FILE f ON f.`FAQ_FILE_ID` = ft.`FAQ_F_ID` WHERE t.TAG_NAME LIKE :key LIMIT 10",{replacements: {key: "%"+pattern+"%"}, type: db.sequelize.QueryTypes.SELECT }).then((response)=>{
   
   var list = response.map( item => {
-  return { id: item["FAQ_FILE_ID"], value: item['ARTICLE_NAME'], label: item['ARTICLE_NAME'] };
+  return { id: item["FAQ_FILE_ID"], value: item['ARTICLE_NAME'], label: item['ARTICLE_NAME'] }
 
   })
 
   res.json(list);
   });
 });
-
-
-
-
